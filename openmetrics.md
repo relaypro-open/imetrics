7a908f76b90eaece8bcc9702a478534fce3127f0
Sam Weaver <sweaver@relaypro.com>
2022-02-11 12:20:02 -0500

    Implement histograms compliant with OpenMetrics spec.

--- openmetrics.md
+++ openmetrics.md
@@ -11,17 +11,19 @@
   - [ ] Include `TYPE` metric descriptor
     - [x] `counter`
     - [x] `gauge`
-    - [ ] `histogram`
-    - [ ] `gaugehistogram`
-    - [ ] `stateset`
-    - [ ] `info`
+    - [x] `histogram`
     - [x] `unknown`
   - [ ] Update handling of mapped data to convert mapped counters and stats maps to tagged metric sets
 - [x] Convert from `mod_esi` to `cowboy` to surface data at `/metrics` URL path (required by the standard)
 - [ ] Extend `imetrics` to support additional OpenMetrics features
-  - [ ] `UNIT` metric descriptor
   - [ ] `HELP` metric descriptor
+  - [ ] `UNIT` metric descriptor
   - [ ] Tags on a per-metric level
   - [ ] Exemplars
+  - [ ] New metric types?
+    - [ ] `gaugehistogram` (or realistically, providing a way to specify histogram type in `imetrics_hist` module)
+    - [ ] `summary` (as a way to surface precomputed percentiles?)
+    - [ ] `stateset`
+    - [ ] `info`
   - [ ] More?
 - [ ] (Optionally) Extend `imetrics` to surface OpenMetrics data via [Protobuf](https://github.com/OpenObservability/OpenMetrics/blob/main/specification/OpenMetrics.md#protobuf-format) instead of UTF-8 text
--- src/openmetrics.erl
+++ src/openmetrics.erl
@@ -11,51 +11,23 @@ terminate(_Reason, _Req, _State) ->
     ok.
 
 get(Req) ->
-    handle(Req, fun imetrics:get_with_types/0).
+    Req1 = cowboy_req:set_resp_header(
+        <<"content-type">>, "application/openmetrics-text", Req
+    ),
+    Req2 = cowboy_req:stream_reply(200, Req1),
+    {ok, Req3} = handle(types, Req2, fun imetrics:get_with_types/0),
+    {ok, Req4} = handle(hist, Req3, fun imetrics_hist:get_all_nobin/0),
+    cowboy_req:stream_body("# EOF\n", nofin, Req4),
+    {ok, cowboy_req:stream_trailers(#{}, Req4)}.
 
-handle(Req, DataFun) ->
+handle(Type, Req, DataFun) ->
     try
-        case erlang:fun_info(DataFun, arity) of
-            {arity, 2} ->
-                %% Stream
-                Req1 = cowboy_req:set_resp_header(
-                    <<"content-type">>, "application/openmetrics-text", Req
-                ),
-                Req2 = cowboy_req:stream_reply(200, Req1),
-                DataFun(
-                    fun(Data, Acc0) ->
-                        deliver_data(Req2, [Data]),
-                        Acc0 + 1
-                    end,
-                    0
-                ),
-                cowboy_req:stream_trailers(#{}, Req2),
-                {ok, Req2};
-            {arity, 0} ->
-                %% Dump
-                case DataFun() of
-                    [] ->
-                        Req1 = cowboy_req:set_resp_header(
-                            <<"content-type">>, "application/openmetrics-text", Req
-                        ),
-                        {ok, cowboy_req:reply(200, #{}, "# EOF\n", Req1)};
-                    {DataHeaders, Data} ->
-                        Req1 = cowboy_req:set_resp_headers(DataHeaders, Req),
-                        Req2 = cowboy_req:stream_reply(200, Req1),
-                        deliver_data(Req2, Data),
-                        cowboy_req:stream_body("# EOF\n", nofin, Req2),
-                        cowboy_req:stream_trailers(#{}, Req2),
-                        {ok, Req2};
-                    Data ->
-                        Req1 = cowboy_req:set_resp_header(
-                            <<"content-type">>, "application/openmetrics-text", Req
-                        ),
-                        Req2 = cowboy_req:stream_reply(200, Req1),
-                        deliver_data(Req2, Data),
-                        cowboy_req:stream_body("# EOF\n", nofin, Req2),
-                        cowboy_req:stream_trailers(#{}, Req2),
-                        {ok, Req2}
-                end
+        case DataFun() of
+            [] ->
+                {ok, Req};
+            Data ->
+                ok = deliver_data(Req, Type, Data),
+                {ok, Req}
         end
     catch
         Class:Reason:Stacktrace ->
@@ -64,11 +36,86 @@ handle(Req, DataFun) ->
             ok
     end.
 
-deliver_data(Req, Data) ->
-    lists:foreach(
-        fun(Value) -> deliver_metricfamily(Req, Value) end,
-        Data
-    ).
+deliver_data(Req, Type, Data) ->
+    Func =
+        case Type of
+            types -> fun deliver_metricfamily/2;
+            hist -> fun deliver_hist/2
+        end,
+    lists:foreach(fun(Value) -> Func(Req, Value) end, Data),
+    ok.
+
+% this function gets the "less than or equal" value for a particular bucket
+% this special case up top is to allow the last bucket to encompass all values
+hist_get_bucket_max(N, {NumBuckets, _}) when N =:= NumBuckets + 1 ->
+    "+Inf";
+hist_get_bucket_max(N, {NumBuckets, [Min, Max]}) when N =< NumBuckets ->
+    [_, BucketMax] = imetrics_hist:compute_bounding_value([Min, Max], NumBuckets, N),
+    strnum(BucketMax).
+
+% the basic function to deliver a histogram. it computes the bucket stats, sorts the
+% buckets, and passes it to the recursive version
+deliver_hist(Req, {Name, Data}) ->
+    cowboy_req:stream_body(["# TYPE ", Name, " histogram\n"], nofin, Req),
+
+    % retrieve the bucket statistics
+    NumBuckets = proplists:get_value(<<"n">>, Data),
+    [Min, Max] = [proplists:get_value(<<"m">>, Data), proplists:get_value(<<"M">>, Data)],
+
+    % sort the list. also, if we're missing a "+Inf" bucket, add it.
+    SortedList =
+        case proplists:is_defined(NumBuckets + 1, Data) of
+            true -> lists:sort(Data);
+            false -> lists:sort([{NumBuckets + 1, 0} | Data])
+        end,
+
+    deliver_hist(Req, Name, {NumBuckets, [Min, Max]}, {_Acc = 0, _PrevBucket = -1, SortedList}).
+
+% the recursive version, which delivers each bucket and recurses, keeping track
+% of an accumulator, since each bucket should include the tally of the previous.
+deliver_hist(Req, Name, HistStats, {Acc, PrevBucket, [_Bucket = {N, Value} | Tail]}) when
+    is_integer(N)
+->
+    % if we jumped over more than one bucket, include the bucket immediately before
+    % the current bucket to minimize uncertainty
+    case (PrevBucket < N - 1) and (Value > 0) of
+        true ->
+            cowboy_req:stream_body(
+                bucket_line(Name, hist_get_bucket_max(N - 1, HistStats), integer_to_binary(Acc)),
+                nofin,
+                Req
+            );
+        false ->
+            noop
+    end,
+
+    % print the current bucket regardless
+    cowboy_req:stream_body(
+        bucket_line(Name, hist_get_bucket_max(N, HistStats), integer_to_binary(Acc + Value)),
+        nofin,
+        Req
+    ),
+
+    % recurse
+    deliver_hist(Req, Name, HistStats, {Acc + Value, N, Tail});
+% skip the entries in the list with binary keys, they're stats about the histogram which we've already processed
+deliver_hist(Req, Name, HistStats, {Acc, PrevBucket, [_Bucket = {N, _Value} | Tail]}) when
+    is_binary(N)
+->
+    deliver_hist(Req, Name, HistStats, {Acc, PrevBucket, Tail});
+% base case, all buckets have been printed.
+deliver_hist(_Req, _Name, _HistStats, {_Acc, _PrevBucket, []}) ->
+    ok.
+
+bucket_line(Name, BucketMax, Value) ->
+    [
+        Name,
+        "_bucket{le=\"",
+        BucketMax,
+        "\"} ",
+        Value,
+        "\n"
+    ].
 
 deliver_metricfamily(Req, {Type, {Name, MetricValue}}) ->
     case Type of
@@ -86,7 +133,8 @@ deliver_metricfamily(Req, {Type, {Name, MetricValue}}) ->
         MappedValues when is_list(MappedValues) ->
             MIo = serialize_proplist(MappedValues),
             cowboy_req:stream_body([<<Name/binary, " ">>, MIo, "\n"], nofin, Req)
-    end.
+    end,
+    ok.
 
 serialize_proplist(List) ->
     MIo = lists:map(
