1080524fa37d07dae642a845406ac446fadf4b8a
Sam Weaver <sweaver@relaypro.com>
2022-02-11 13:31:51 -0500

    Revert formatting changes from previous commit.
    (My autoformatter made a large amount of changes which dirties Git history, reverting these for now.)

--- test/imetrics_tests.erl
+++ test/imetrics_tests.erl
@@ -12,10 +12,13 @@ stop(_Fixture) ->
 
 %% add tests
 add_test_() ->
-    {setup, fun start/0, fun stop/1, fun add_test/1}.
+    {setup,
+        fun start/0,
+        fun stop/1,
+        fun add_test/1}. 
 
 add_test(_Fixture) ->
-    [imetrics:add(many_incr) || _ <- lists:seq(1, 4)],
+    [ imetrics:add(many_incr) || _ <- lists:seq(1, 4) ],
     [
         ?_assertEqual(1, imetrics:add(atom)),
         ?_assertEqual(5, imetrics:add(atom_set, 5)),
@@ -31,19 +34,20 @@ add_test(_Fixture) ->
         ?_assertEqual(5, imetrics:add_m("list_m", "list_k_set", 5)),
         ?_assertEqual(1, imetrics:add_m(atom_m, 200)),
         ?_assertEqual(5, imetrics:add(many_incr)),
-        ?_assertEqual(1, imetrics:add({a, b, c})),
+        ?_assertEqual(1, imetrics:add({a,b,c})),
         ?_assertEqual(1, imetrics:add(200)),
-        ?_assertEqual(
-            {error, {function_clause, check_inputs}}, imetrics:add({a, b, c, d, e, f, g, h, i})
-        ),
-        ?_assertEqual({error, {badmatch, check_inputs}}, imetrics:add(bad, map))
+        ?_assertEqual({error,{function_clause,check_inputs}}, imetrics:add({a,b,c,d,e,f,g,h,i})),
+        ?_assertEqual({error,{badmatch,check_inputs}}, imetrics:add(bad, map))
     ].
 
 gauge_test_() ->
-    {setup, fun start/0, fun stop/1, fun gauge_test/1}.
+    {setup,
+        fun start/0,
+        fun stop/1,
+        fun gauge_test/1}.
 
 gauge_test(_Fixture) ->
-    [imetrics:set_gauge(many_set, X) || X <- lists:seq(1, 4)],
+    [ imetrics:set_gauge(many_set, X) || X <- lists:seq(1, 4) ],
     F = 1.5,
     [
         ?_assertEqual(F, imetrics:set_gauge(atom, F)),
@@ -62,10 +66,7 @@ multigauge_test(_) ->
     imetrics:set_multigauge(multigauge_test, dim, fun() -> [{<<"test">>, 3}] end),
     Gauges = imetrics:get_gauges(),
     [
-        ?_assertEqual(
-            [{<<"$dim">>, <<"dim">>}, {<<"test">>, 3}],
-            proplists:get_value(<<"multigauge_test">>, Gauges)
-        )
+     ?_assertEqual([{<<"$dim">>,<<"dim">>},{<<"test">>,3}], proplists:get_value(<<"multigauge_test">>, Gauges))
     ].
 
 dimension_test_() ->
@@ -76,31 +77,34 @@ dimension_test(_) ->
     imetrics:add_m(dimension_test2, key2),
     imetrics:set_counter_dimension(dimension_test2, dim2),
     [
-        ?_assertEqual(
-            [
-                {<<"dimension_test1">>, [{<<"$dim">>, <<"dim1">>}]},
-                {<<"dimension_test2">>, [{<<"$dim">>, <<"dim2">>}, {<<"key2">>, 1}]}
-            ],
-            imetrics:get_counters()
-        )
+     ?_assertEqual([{<<"dimension_test1">>,[{<<"$dim">>,<<"dim1">>}]},
+                  {<<"dimension_test2">>,
+                   [{<<"$dim">>,<<"dim2">>},{<<"key2">>,1}]}], imetrics:get_counters())
     ].
 
 %% badarg tests
 badarg_test_() ->
-    {setup, fun() -> ok end, fun(_) -> ok end, fun badarg_test/1}.
+    {setup,
+        fun() -> ok end,
+        fun(_) -> ok end,
+        fun badarg_test/1}.
 
 badarg_test(_Fixture) ->
     % Make sure we don't crash if ets tables do not exist
     [
-        ?_assertEqual({error, {badarg, check_ets}}, imetrics:add(atom)),
-        ?_assertEqual({error, {badarg, check_ets}}, imetrics:add_m(atom_m, k)),
-        ?_assertEqual({error, {badarg, check_ets}}, imetrics:set_gauge(atom_g, 1.5)),
-        ?_assertEqual({error, {badarg, check_ets}}, imetrics:set_gauge_m(atom_gm, k, 1.5))
+        ?_assertEqual({error,{badarg,check_ets}}, imetrics:add(atom)),
+        ?_assertEqual({error,{badarg,check_ets}}, imetrics:add_m(atom_m, k)),
+        ?_assertEqual({error,{badarg,check_ets}}, imetrics:set_gauge(atom_g, 1.5)),
+        ?_assertEqual({error,{badarg,check_ets}}, imetrics:set_gauge_m(atom_gm, k, 1.5))
     ].
+
 
 %% get tests
 get_test_() ->
-    {setup, fun start_get/0, fun stop/1, fun get_test/1}.
+    {setup,
+        fun start_get/0,
+        fun stop/1,
+        fun get_test/1}. 
 
 start_get() ->
     F = start(),
@@ -119,21 +123,20 @@ get_test(_Fixture) ->
     Data = imetrics:get(),
     [
         ?_assertEqual(1, proplists:get_value(<<"counter">>, Data)),
-        ?_assertEqual(
-            [{<<"key">>, 1}],
-            proplists:get_value(<<"mapped_counter">>, Data)
-        ),
+        ?_assertEqual([{<<"key">>, 1}],
+            proplists:get_value(<<"mapped_counter">>, Data)),
         ?_assertEqual(1.5, proplists:get_value(<<"gauge">>, Data)),
-        ?_assertEqual(
-            [{<<"key">>, 1.5}],
-            proplists:get_value(<<"mapped_gauge">>, Data)
-        ),
+        ?_assertEqual([{<<"key">>, 1.5}],
+            proplists:get_value(<<"mapped_gauge">>, Data)),
         ?_assertEqual(1.5, proplists:get_value(<<"fn_gauge">>, Data))
     ].
 
 %% http tests
 http_test_() ->
-    {setup, fun start_http/0, fun stop/1, fun http_test/1}.
+    {setup,
+        fun start_http/0,
+        fun stop/1,
+        fun http_test/1}. 
 
 start_http() ->
     F = start_get(),
@@ -145,20 +148,18 @@ http_test(#{port := Port}) ->
     http_test_varz(#{port => Port}) ++ http_test_openmetrics(#{port => Port}).
 
 http_test_varz(#{port := Port}) ->
-    Url = "http://localhost:" ++ integer_to_list(Port) ++ "/imetrics/varz:get",
-    {ok, {Status, _Headers, Body}} =
+    Url = "http://localhost:"++integer_to_list(Port)++"/imetrics/varz:get",
+    {ok, {Status, _Headers, Body}} = 
         httpc:request(get, {Url, []}, [], []),
     {_Vsn, Code, _Friendly} = Status,
 
-    Res = [
-        "counter 1",
+    Res = ["counter 1",
         "counter_tuple 1",
         "counter:tuple:colon 1",
         "fn_gauge 1.5",
         "gauge 1.5",
         "mapped_counter key:1",
-        "mapped_gauge key:1.5"
-    ],
+        "mapped_gauge key:1.5"],
     Res2 = string:join(Res, "\n") ++ "\n",
     [
         ?_assertEqual(200, Code),
@@ -195,7 +196,10 @@ http_test_openmetrics(#{port := Port}) ->
 
 %% tick/tock tests
 ticktock_test_() ->
-    {setup, fun start_ticktock/0, fun stop/1, fun ticktock_test/1}.
+    {setup,
+        fun start_ticktock/0,
+        fun stop/1,
+        fun ticktock_test/1}. 
 
 start_ticktock() ->
     F = start(),
@@ -205,47 +209,38 @@ start_ticktock() ->
 
 ticktock_test(_Fixture) ->
     [
-        ?_assertEqual(
-            {15, 1},
-            (fun() ->
-                Tick = imetrics:tick(test, millisecond),
-                timer:sleep(285),
-                imetrics:tock(Tick)
-            end)()
-        ),
-        ?_assertEqual(
-            {15, 1},
-            (fun() ->
-                Tick = imetrics:tick(test, millisecond),
-                timer:sleep(285),
-                imetrics:tock_as(Tick, test2)
-            end)()
-        )
+        ?_assertEqual({15, 1}, (fun() ->
+                    Tick = imetrics:tick(test, millisecond),
+                    timer:sleep(285),
+                    imetrics:tock(Tick)
+            end)()),
+        ?_assertEqual({15, 1}, (fun() ->
+                    Tick = imetrics:tick(test, millisecond),
+                    timer:sleep(285),
+                    imetrics:tock_as(Tick, test2)
+            end)())
     ].
 
 ticktock_s_test_() ->
-    {setup, fun start_ticktock/0, fun stop/1, fun ticktock_s_test/1}.
+    {setup,
+        fun start_ticktock/0,
+        fun stop/1,
+        fun ticktock_s_test/1}. 
 
 ticktock_s_test(_Fixture) ->
     [
-        ?_assertMatch(
-            {15, 1},
-            (fun() ->
-                {Ref, Ticks} = imetrics:tick_s(#{}, test, millisecond),
-                timer:sleep(285),
-                {Result, Ticks2} = imetrics:tock_s(Ticks, Ref),
-                0 = map_size(Ticks2),
-                Result
-            end)()
-        ),
-        ?_assertMatch(
-            {15, 2},
-            (fun() ->
-                {_Ref, Ticks} = imetrics:tick_s(#{}, test, millisecond),
-                timer:sleep(285),
-                {Result, Ticks2} = imetrics:tock_s(Ticks, test),
-                0 = map_size(Ticks2),
-                Result
-            end)()
-        )
+     ?_assertMatch({15, 1}, (fun() ->
+                                     {Ref, Ticks} = imetrics:tick_s(#{}, test, millisecond),
+                                     timer:sleep(285),
+                                     {Result, Ticks2} = imetrics:tock_s(Ticks, Ref),
+                                     0 = map_size(Ticks2),
+                                     Result
+                             end)()),
+     ?_assertMatch({15, 2}, (fun() ->
+                                     {_Ref, Ticks} = imetrics:tick_s(#{}, test, millisecond),
+                                     timer:sleep(285),
+                                     {Result, Ticks2} = imetrics:tock_s(Ticks, test),
+                                     0 = map_size(Ticks2),
+                                     Result
+                             end)())
     ].
