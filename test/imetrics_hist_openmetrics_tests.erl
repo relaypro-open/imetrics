2d5ad4fca4387e6f7f3a35f0a23598c14e105870
Hiimadd <addisoncmorse@gmail.com>
2023-06-13 12:22:19 -0400

    Limit bucket count, fix hist range constructor
    Added an environment variable to limit the maximum number of buckets allowed in a histogram as a failsafe if people unintentionally create massive histograms, mostly useful to prevent cost blowups as each bucket is considered a separate time series. Additionally, discovered and fixed a bug with the range based constructor for histograms where 1. the min bucket would sometimes not be created due to floating point errors, and 2. more buckets would sometimes be generated than intended. Added/modified tests to account for both the new feature and the bug fix.

--- test/imetrics_hist_openmetrics_tests.erl
+++ test/imetrics_hist_openmetrics_tests.erl
@@ -25,12 +25,16 @@ new_test(_Fixture) ->
         ?_assertEqual(true, imetrics_hist_openmetrics:new(longer_list, [0, 0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.4, 1, 3, 10, 100, 100000])),
         ?_assertEqual(true, imetrics_hist_openmetrics:new(multiple_tags, #{test_tag => "test_value", another_test_tag => "another_test_value"}, [0,1,2])),
         ?_assertEqual(true, imetrics_hist_openmetrics:new(single_range, [0,0], 1)),
-        ?_assertEqual(true, imetrics_hist_openmetrics:new(larger_range, [0,1000], 101)),
-        ?_assertEqual(true, imetrics_hist_openmetrics:new(decimal_range, [0, 1], 1001)),
         ?_assertEqual(true, imetrics_hist_openmetrics:new(empty_range, [0,0], 0)),
         ?_assertEqual(true, imetrics_hist_openmetrics:new(larger_empty_range, [0,100], 0)),
         ?_assertEqual({error, {badarith, check_inputs}}, imetrics_hist_openmetrics:new(illegal_range, [0,1], 1)),
-        ?_assertEqual({error, {badarith, check_inputs}}, imetrics_hist_openmetrics:new(zero_range, [0,0], 2))
+        ?_assertEqual({error, {badarith, check_inputs}}, imetrics_hist_openmetrics:new(zero_range, [0,0], 2)),
+
+        ?_assertEqual({error, {badarg, check_ets}}, imetrics_hist_openmetrics:new(oversize_range, [0, 1], 65)),
+        ?_assertEqual(ok, application:set_env(imetrics, hist_max_buckets, 8)),
+        ?_assertEqual({error, {badarg, check_ets}}, imetrics_hist_openmetrics:new(oversize_dynamic, [1, 2, 3, 4, 5, 6, 7, 8, 9])),
+        ?_assertEqual(ok, application:set_env(imetrics, hist_max_buckets, 64)),
+        ?_assertEqual(true, imetrics_hist_openmetrics:new(oversize_dynamic, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
     ].
 
 add_test_() ->
@@ -69,7 +73,6 @@ get_test_() ->
 
 get_test(_Fixture) ->
     [
-        %doesn't test get_hist(Identifier) explicitly, as it's called by get_all(), and it isn't expected to be called by users directly.
         ?_assertEqual(true, imetrics_hist_openmetrics:new(simple, [0])),
         ?_assertEqual([{#{le => <<"0">>}, 0},{#{le => <<"+Inf">>}, 0}], imetrics_hist_openmetrics:get_hist(simple)),
         ?_assertEqual([], imetrics_hist_openmetrics:get_hist(fake_hist)),
@@ -115,7 +118,7 @@ get_test(_Fixture) ->
         ?_assertEqual({3,1}, imetrics_hist_openmetrics:add(tagged, #{tag => "one"}, 2)),
         ?_assertEqual({3,2}, imetrics_hist_openmetrics:add(tagged, #{tag => "one"}, 2)),
         ?_assertEqual({4,1}, imetrics_hist_openmetrics:add(tagged, #{tag => "one"}, 5)),
-                ?_assertEqual([{<<"tagged">>,
+        ?_assertEqual([{<<"tagged">>,
                         [{#{le => <<"1">>, tag => <<"two">>}, 0},
                         {#{le => <<"1.01">>, tag => <<"two">>}, 0},
                         {#{le => <<"1.1">>, tag => <<"two">>}, 0},
@@ -129,7 +132,38 @@ get_test(_Fixture) ->
                         {#{le => <<"+Inf">>, tag => <<"one">>}, 4}]},
                        {<<"simple">>,
                         [{#{le => <<"0">>}, 1},
-                        {#{le => <<"+Inf">>}, 2}]}], imetrics_hist_openmetrics:get_all())
+                        {#{le => <<"+Inf">>}, 2}]}], imetrics_hist_openmetrics:get_all()),
+        ?_assertEqual(true, imetrics_hist_openmetrics:new(range, [15, 20], 6)),
+        ?_assertEqual([{#{le => <<"15">>}, 0},
+                       {#{le => <<"16.0">>}, 0},
+                       {#{le => <<"17.0">>}, 0},
+                       {#{le => <<"18.0">>}, 0},
+                       {#{le => <<"19.0">>}, 0},
+                       {#{le => <<"20">>}, 0},
+                       {#{le => <<"+Inf">>}, 0}], imetrics_hist_openmetrics:get_hist(range)),
+        ?_assertEqual(true, imetrics_hist_openmetrics:new(large_range, [0, 1], 21)),
+        ?_assertEqual([{#{le => <<"0">>},0},
+                       {#{le => <<"0.049999999999999684">>},0},
+                       {#{le => <<"0.09999999999999969">>},0},
+                       {#{le => <<"0.1499999999999997">>},0},
+                       {#{le => <<"0.19999999999999968">>},0},
+                       {#{le => <<"0.24999999999999967">>},0},
+                       {#{le => <<"0.29999999999999966">>},0},
+                       {#{le => <<"0.34999999999999964">>},0},
+                       {#{le => <<"0.39999999999999963">>},0},
+                       {#{le => <<"0.4499999999999996">>},0},
+                       {#{le => <<"0.4999999999999996">>},0},
+                       {#{le => <<"0.5499999999999996">>},0},
+                       {#{le => <<"0.5999999999999996">>},0},
+                       {#{le => <<"0.6499999999999997">>},0},
+                       {#{le => <<"0.6999999999999997">>},0},
+                       {#{le => <<"0.7499999999999998">>},0},
+                       {#{le => <<"0.7999999999999998">>},0},
+                       {#{le => <<"0.8499999999999999">>},0},
+                       {#{le => <<"0.8999999999999999">>},0},
+                       {#{le => <<"0.95">>},0},
+                       {#{le => <<"1">>},0},
+                       {#{le => <<"+Inf">>},0}], imetrics_hist_openmetrics:get_hist(large_range))
     ].
 
 exemplar_test_() ->
